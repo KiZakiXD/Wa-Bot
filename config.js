beb4db274cf26d1698be2989bd7e573f949ad668
KiZakiXD <88037146+KiZakiXD@users.noreply.github.com>
2022-02-05 19:39:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ global.packName = '𝙺𝙸𝚉𝙰𝙺𝙸𝚇𝙳'
 global.authorName = 'Yotsuba - Bot'
 global.prefix = '#'
 global.mode = 'publik'
-global.ownerNumber = ['87813379','6285878313791']
+global.ownerNumber = ['687813379','6285878313791']
 global.thumb = fs.readFileSync('./thumb.jpeg')
 
 global.APIs = {
